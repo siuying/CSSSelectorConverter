31bc716d3259df94b4761415bb41383aff3e4b20
Francis Chong <francis@ignition.hk>
2014-01-22 17:39:55 +0800

    add docs

--- CSSSelectorConverter/CSSSelectorXPathVisitor.h
+++ CSSSelectorConverter/CSSSelectorXPathVisitor.h
@@ -10,10 +10,25 @@
 
 @class CSSBaseSelector;
 
+/**
+ * Walk a tree of CSSBaseSelector subclasses, and build a XPath representation of the CSS.
+ * 
+ * Upon finish, invoke xpathString to get the XPath.
+ */
 @interface CSSSelectorXPathVisitor : NSObject
 
+
+/**
+ * Visit a selector node (CSSBaseSelector subclasses) and generate XPath representation of the CSS Selector.
+ *
+ * @param node A CSSBaseSelector subclasses.
+ * @throw NSInvalidArgumentException if the class is not supported by the visitor.
+ */
 -(void) visit:(CSSBaseSelector*)node;
 
+/**
+ * @return Returns XPath representation of the CSS Selector.
+ */
 -(NSString*) xpathString;
 
 @end
